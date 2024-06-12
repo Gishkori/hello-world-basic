1b5d0ffc6f0191518509ec9829c41af22d4fcbd5
Ali Asghar Gishkori <gishkori@hotmail.com>
2024-06-12 20:02:02 +1000

    Project Setup
    Did an initial setup of the project

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"h/jYBJQoysx68UfupsMm0ahRhfEVME2lrWDbVve/MiM=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Welcome to my App</h1>
+                  <MyButton />
+        </div>
+    );
+}
